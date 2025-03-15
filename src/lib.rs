2a7cdf038fdc9ac5bf91cca29262d8378df8f7ac
Kanji Tanaka <ktanaka@ichain.co.jp>
2025-03-16 07:26:45 +0900

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,37 @@ fn search(line: &str, pattern: &str) -> bool {
     match pattern {
         "\\d" => line.chars().any(|c| c.is_ascii_digit()),
         "\\w" => line.chars().any(|c| c.is_ascii_alphanumeric() || c == '_'),
+        p if p.starts_with("[^") && p.ends_with("]") => {
+            let group = &p[2..p.len() - 1];
+            group
+                .chars()
+                .all(|c| !search(line, format!("{c}").as_str()))
+        }
         p if p.starts_with("[") && p.ends_with("]") => {
             let group = &p[1..p.len() - 1];
             group.chars().any(|c| search(line, format!("{c}").as_str()))
         }
         _ => line.contains(pattern),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_searches_with_positive_group() {
+        let pattern = "[abcd]";
+        assert!(search("a", pattern));
+        assert!(!search("efgh", pattern));
+    }
+
+    #[test]
+    fn it_searches_with_negative_group() {
+        let pattern = "[^xyz]";
+        assert!(search("apple", pattern));
+
+        let pattern = "[^anb]";
+        assert!(!search("banana", pattern));
     }
 }
